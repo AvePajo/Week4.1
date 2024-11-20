d9bbfd5c14d6ce938a0aba7626fc2a58548f1573
Ave Pajo <ave.pajo@tktk.ee>
2024-11-21 00:03:14 +0200

    Week4.1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Random rnd = new Random(); // juhuslike arvude klass
+
+int myRandom;    // juhuslik arv
+int i;          // Loendur
+
+
+
+for (i = 0; i < 5; i++)
+{
+    myRandom = rnd.Next(1, 11); // juhuslik 1..10
+    Console.WriteLine($"{i + 1}. {myRandom}");
+}
+
+
+//for(int i = 0; i< 3; i++)
+//{
+//    Console.WriteLine($"{i + 1 }. I will not skip Zoom classes.");
+//}
